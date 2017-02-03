82bcfbc12bec75d10f4e2a4b7a303a9f168a000c
vrichard <Pa$$W0rd>
2017-02-03 10:02:57 +0100

    add min files

--- event-listener-polyfill.js
+++ event-listener-polyfill.js
@@ -1,27 +1,17 @@
 (function() {
 
-  var EventListenerHelper = {};
-
   /**
    * Create EventTarget
    */
-  EventListenerHelper.polyfillEventTarget = function() {
-    if(typeof EventTarget === 'undefined') {
-      window.EventTarget = Node;
-    }
-  };
+  if(typeof EventTarget === 'undefined') {
+    window.EventTarget = Node;
+  }
 
   /**
    * Check supported types
    */
 
-  EventListenerHelper.prefixEventType = function(type) {
-    return ['', 'webkit', 'moz', 'ms', 'o'].map(function(prefix) {
-      return prefix + type;
-    });
-  };
-
-  EventListenerHelper.eventTypes = {
+  var eventTypes = {
     wheel: ['wheel', 'mousewheel', 'DOMMouseScroll']
   };
 
@@ -31,139 +21,15 @@
     'animationend', 'animationiteration', 'animationstart', 'transitionend',
     'pointercancel', 'pointerdown', 'pointerhover', 'pointermove', 'pointerout', 'pointerover', 'pointerup'
   ].forEach(function(type) {
-    EventListenerHelper.eventTypes[type] = EventListenerHelper.prefixEventType(type);
+    EventListenerHelper.eventTypes[type] = ['', 'webkit', 'moz', 'ms', 'o']
+      .map(function(prefix) {
+        return prefix + type;
+      });
   });
 
-
-  /**
-   * Check supported options
-   */
-  EventListenerHelper.supportedOptions = {
-    once: false,
-    passive: false,
-    capture: false
-  };
-
-  EventListenerHelper.getSupportedOptions = function() {
-    document.createDocumentFragment().addEventListener('test', function() {}, {
-      get once() {
-        EventListenerHelper.supportedOptions.once = true;
-        return false;
-      },
-      get passive() {
-        EventListenerHelper.supportedOptions.passive = true;
-        return false;
-      },
-      get capture() {
-        EventListenerHelper.supportedOptions.capture = true;
-        return false;
-      }
-    });
-
-    // useful shortcuts to detect if options are all/some supported
-    EventListenerHelper.supportedOptions.all  = EventListenerHelper.supportedOptions.once && EventListenerHelper.supportedOptions.passive && EventListenerHelper.supportedOptions.capture;
-    EventListenerHelper.supportedOptions.some = EventListenerHelper.supportedOptions.once || EventListenerHelper.supportedOptions.passive || EventListenerHelper.supportedOptions.capture;
-  };
-
-
-  /**
-   * Polyfill listener
-   */
-  EventListenerHelper.getFormattedListener = function(listener) {
-    if(typeof listener === 'function') {
-      return listener;
-    } else if((typeof listener === 'object') && (typeof listener.handleEvent === 'function')) {
-      return listener.handleEvent;
-    } else {
-      // to support Symbol
-      return function(event) {
-        listener(event);
-      };
-    }
-  };
-
-  EventListenerHelper.getFormattedOptions = function(options) {
-    switch(typeof options) {
-      case 'boolean':
-        options = { capture: options };
-        break;
-      case 'undefined':
-        options = { capture: false };
-        break;
-      case 'object':
-        break;
-      default:
-        throw new Error('Unsupported options type for addEventListener');
-        break;
-    }
-
-    options.once      = (typeof options.once === 'boolean') ? options.once : false;
-    options.passive   = (typeof options.passive === 'boolean') ? options.passive : false;
-    options.capture   = (typeof options.capture === 'boolean') ? options.capture : false;
-    options.polyfill  = (typeof options.polyfill === 'boolean') ? options.polyfill : true;
-
-    return options;
-  };
-
-  EventListenerHelper.getFormattedArguments = function(type, listener, options) {
-    return {
-      type: type,
-      listener: EventListenerHelper.getFormattedListener(listener),
-      options: EventListenerHelper.getFormattedOptions(options)
-    };
-  };
-
-
-  EventListenerHelper.registerEventListener = function(target, formattedArguments) {
-    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
-    if(typeof target.__eventListeners === 'undefined') {
-      target.__eventListeners = {};
-    }
-    if(typeof target.__eventListeners[key] === 'undefined') {
-      target.__eventListeners[key] = [];
-    }
-    target.__eventListeners[key].push(formattedArguments);
-  };
-
-  EventListenerHelper.unregisterEventListener = function(target, formattedArguments) {
-    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
-    if(
-      (typeof target.__eventListeners !== 'undefined') &&
-      (typeof target.__eventListeners[key] !== 'undefined')
-    ) {
-      var map = target.__eventListeners[key];
-      for(var i = 0; i < map.length; i++) {
-        if(map[i].listener === formattedArguments.listener) {
-          map.splice(i, 1);
-        }
-      }
-
-      if(map.length === 0) {
-        delete target.__eventListeners[key];
-      }
-    }
-  };
-
-  EventListenerHelper.getRegisteredEventListener = function(target, formattedArguments) {
-    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
-    if(
-      (typeof target.__eventListeners !== 'undefined') &&
-      (typeof target.__eventListeners[key] !== 'undefined')
-    ) {
-      var map = target.__eventListeners[key];
-      for(var i = 0; i < map.length; i++) {
-        if(map[i].listener === formattedArguments.listener) {
-          return map[i];
-        }
-      }
-    }
-    return null;
-  };
-
-
-  EventListenerHelper.polyfillEventTypesName = function(type, target) {
-    var eventTypesPolyfiller = EventListenerHelper.eventTypes[type];
-    if(typeof eventTypesPolyfiller === 'undefined') {
+  var polyfillEventTypesName = function(type, target) {
+    var eventTypesPolyfiller = eventTypes[type];
+    if(eventTypesPolyfiller === void 0) {
       return type;
     } else {
       var i = 0;
@@ -180,7 +46,42 @@
   };
 
 
-  EventListenerHelper.keyCodes = [
+
+  /**
+   * Check supported options
+   */
+  var supportedOptions = {
+    once: false,
+    passive: false,
+    capture: false
+  };
+
+
+  document.createDocumentFragment().addEventListener('test', function() {}, {
+    get once() {
+      supportedOptions.once = true;
+      return false;
+    },
+    get passive() {
+      supportedOptions.passive = true;
+      return false;
+    },
+    get capture() {
+      supportedOptions.capture = true;
+      return false;
+    }
+  });
+
+  // useful shortcuts to detect if options are all/some supported
+  supportedOptions.all  = supportedOptions.once && supportedOptions.passive && supportedOptions.capture;
+  supportedOptions.some = supportedOptions.once || supportedOptions.passive || supportedOptions.capture;
+
+
+
+  /**
+   * Check supported events attributes
+   */
+  var keyCodes = [
     null,
     null,
     null,
@@ -410,47 +311,130 @@
     "IntlBackslash"
   ];
 
-  window.buildKeyCodes = function() {
-    window.addEventListener('keydown', function(event) {
-      if(EventListenerHelper.keyCodes[event.keyCode] && EventListenerHelper.keyCodes[event.keyCode] !== event.code) {
-        console.warn('Detect same keyCode for 2 different codes : ' + event.code + ' (current) vs ' + EventListenerHelper.keyCodes[event.keyCode] + '(old)');
+  // window.buildKeyCodes = function() {
+  //   window.addEventListener('keydown', function(event) {
+  //     if(EventListenerHelper.keyCodes[event.keyCode] && EventListenerHelper.keyCodes[event.keyCode] !== event.code) {
+  //       console.warn('Detect same keyCode for 2 different codes : ' + event.code + ' (current) vs ' + EventListenerHelper.keyCodes[event.keyCode] + '(old)');
+  //     }
+  //     EventListenerHelper.keyCodes[event.keyCode] = event.code;
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   });
+  // };
+  //
+  // window.endBuildKeyCodes = function() {
+  //   console.log(JSON.stringify(EventListenerHelper.keyCodes, null, '\t'));
+  // };
+
+
+
+  /**
+   * Polyfill listener
+   */
+
+    /**
+     * Formatter functions
+     */
+  var getFormattedListener = function(listener) {
+    if(typeof listener === 'function') {
+      return listener;
+    } else if((typeof listener === 'object') && (typeof listener.handleEvent === 'function')) {
+      return listener.handleEvent;
+    } else {
+      // to support Symbol
+      return function(event) {
+        listener(event);
+      };
+    }
+  };
+
+  var getFormattedOptions = function(options) {
+    switch(typeof options) {
+      case 'boolean':
+        options = { capture: options };
+        break;
+      case 'undefined':
+        options = { capture: false };
+        break;
+      case 'object':
+        break;
+      default:
+        throw new Error('Unsupported options type for addEventListener');
+        break;
+    }
+
+    options.once      = (typeof options.once === 'boolean') ? options.once : false;
+    options.passive   = (typeof options.passive === 'boolean') ? options.passive : false;
+    options.capture   = (typeof options.capture === 'boolean') ? options.capture : false;
+    options.polyfill  = (typeof options.polyfill === 'boolean') ? options.polyfill : true;
+
+    return options;
+  };
+
+  var getFormattedArguments = function(type, listener, options) {
+    return {
+      type: type,
+      listener:getFormattedListener(listener),
+      options: getFormattedOptions(options)
+    };
+  };
+
+
+    /**
+     * Event listener stack functions
+     */
+  var registerEventListener = function(target, formattedArguments) {
+    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
+    if(target.__eventListeners === void 0) {
+      target.__eventListeners = {};
+    }
+    if(arget.__eventListeners[key] === void 0) {
+      target.__eventListeners[key] = [];
+    }
+    target.__eventListeners[key].push(formattedArguments);
+  };
+
+  var unregisterEventListener = function(target, formattedArguments) {
+    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
+    if(
+      (target.__eventListeners !==  void 0) &&
+      (target.__eventListeners[key] !== void 0)
+    ) {
+      var map = target.__eventListeners[key];
+      for(var i = 0; i < map.length; i++) {
+        if(map[i].listener === formattedArguments.listener) {
+          map.splice(i, 1);
+        }
       }
-      EventListenerHelper.keyCodes[event.keyCode] = event.code;
-      event.preventDefault();
-      event.stopPropagation();
-    });
-  };
 
-  window.endBuildKeyCodes = function() {
-    console.log(JSON.stringify(EventListenerHelper.keyCodes, null, '\t'));
-  };
-
-
-  EventListenerHelper.polyfillEventTypesObject = function(event) {
-   if(event instanceof KeyboardEvent) {
-      if(!('code' in event)) {
-        event.code = EventListenerHelper.keyCodes[event.keyCode];
+      if(map.length === 0) {
+        delete target.__eventListeners[key];
       }
     }
   };
 
-  EventListenerHelper.polyfilledConstructors = {};
-
-  EventListenerHelper.polyfillListener = function() {
-    EventListenerHelper.polyfillListenerConstructor(EventTarget);
-    if(!(window instanceof EventTarget)) { EventListenerHelper.polyfillListenerConstructor(Window); }
+  var getRegisteredEventListener = function(target, formattedArguments) {
+    var key = formattedArguments.type + '-' + (formattedArguments.options.capture ? '1' : '0');
+    if(
+      (target.__eventListeners !== void 0) &&
+      (target.__eventListeners[key] !== void 0)
+    ) {
+      var map = target.__eventListeners[key];
+      for(var i = 0; i < map.length; i++) {
+        if(map[i].listener === formattedArguments.listener) {
+          return map[i];
+        }
+      }
+    }
+    return null;
   };
 
-  EventListenerHelper.polyfillListenerConstructor = function(constructor, name) {
-    var polyfilledConstructor = {
-      name: name,
-      addEventListener: constructor.prototype.addEventListener,
-      removeEventListener:  constructor.prototype.removeEventListener
-    };
 
+  var polyfillListenerConstructor = function(constructor) {
+    var addEventListener = constructor.prototype.addEventListener;
     constructor.prototype.addEventListener = function(type, listener, options) {
-      var formattedArguments      = EventListenerHelper.getFormattedArguments(type, listener, options);
-      var registeredEventListener = EventListenerHelper.getRegisteredEventListener(this, formattedArguments);
+      var formattedArguments      = getFormattedArguments(type, listener, options);
+      var registeredEventListener = getRegisteredEventListener(this, formattedArguments);
 
       if(!registeredEventListener) {
 
@@ -458,18 +442,18 @@
 
 
         vendorArguments.type = formattedArguments.options.polyfill ?
-          EventListenerHelper.polyfillEventTypesName(formattedArguments.type) :
+          polyfillEventTypesName(formattedArguments.type) :
           formattedArguments.type
         ;
 
         vendorArguments.listener = function(event) {
             // once
-          if(formattedArguments.options.once && !EventListenerHelper.supportedOptions.once) {
+          if(formattedArguments.options.once && !supportedOptions.once) {
             this.removeEventListener(type, listener, options);
           }
 
             // passive
-          if(formattedArguments.options.passive && !EventListenerHelper.supportedOptions.passive) {
+          if(formattedArguments.options.passive && !supportedOptions.passive) {
             event.preventDefault = function() {
               throw new Error('Unable to preventDefault inside passive event listener invocation.');
             };
@@ -478,22 +462,29 @@
             // polyfill
           if(formattedArguments.options.polyfill) {
             event.type = formattedArguments.type;
-            EventListenerHelper.polyfillEventTypesObject(event);
+
+              // polyfill event attributes
+            if(event instanceof KeyboardEvent) {
+              if(!('code' in event)) {
+                //noinspection JSAnnotator
+                event.code = keyCodes[event.keyCode];
+              }
+            }
           }
 
           return formattedArguments.listener.call(this, event);
         };
 
-        vendorArguments.options = EventListenerHelper.supportedOptions.some ?
+        vendorArguments.options = supportedOptions.some ?
           formattedArguments.options : formattedArguments.options.capture;
 
         formattedArguments.vendorArguments = vendorArguments;
 
         // console.log(formattedArguments);
 
-        EventListenerHelper.registerEventListener(this, formattedArguments);
+        registerEventListener(this, formattedArguments);
 
-        polyfilledConstructor.addEventListener.call(
+        addEventListener.call(
           this,
           vendorArguments.type,
           vendorArguments.listener,
@@ -502,40 +493,31 @@
       }
     };
 
+    var removeEventListener = constructor.prototype.removeEventListener;
     constructor.prototype.removeEventListener = function(type, listener, options) {
-      var formattedArguments      = EventListenerHelper.getFormattedArguments(type, listener, options);
-      var registeredEventListener = EventListenerHelper.getRegisteredEventListener(this, formattedArguments);
+      var formattedArguments      = getFormattedArguments(type, listener, options);
+      var registeredEventListener = getRegisteredEventListener(this, formattedArguments);
 
       if(registeredEventListener) {
-        EventListenerHelper.unregisterEventListener(this, formattedArguments);
-        polyfilledConstructor.removeEventListener.call(
+        unregisterEventListener(this, formattedArguments);
+        removeEventListener.call(
           this,
           registeredEventListener.vendorArguments.type,
           registeredEventListener.vendorArguments.listener,
           registeredEventListener.vendorArguments.options
         );
       } else {
-        polyfilledConstructor.removeEventListener.call(this, type, listener, options);
+        removeEventListener.call(this, type, listener, options);
       }
     };
-
-    EventListenerHelper.polyfilledConstructors[name] = polyfilledConstructor;
-  };
-
-  /**
-   * Polyfill all
-   */
-
-  EventListenerHelper.polyfillAll = function() {
-    EventListenerHelper.polyfillEventTarget();
-    EventListenerHelper.getSupportedOptions();
-    EventListenerHelper.polyfillListener();
   };
 
 
-  EventListenerHelper.polyfillAll();
+  polyfillListenerConstructor(EventTarget);
+  if(!(window instanceof EventTarget)) {
+    polyfillListenerConstructor(Window);
+  }
 
-  window.EventListenerHelper = EventListenerHelper;
 
   // var div = document.createElement('div');
   // document.body.innerHTML = '';
