32d2e76b181a0ace05f6912af3c074923f5acffd
Brian McMurray <brian@brianmcmurray.com>
2019-04-24 16:12:16 -0400

    Remove 'Symbol' support which breaks LodashWrapper event callbacks. Fix logic check for listener.

--- index.js
+++ index.js
@@ -82,16 +82,13 @@ module.exports = (function() {
 
 
   EventListenerInterceptor.normalizeListenerCallback = function(listener) {
-    if (!listener) {
+    if (listener !== null) {
       if(typeof listener === 'function') {
         return listener;
       } else if((typeof listener === 'object') && (typeof listener.handleEvent === 'function')) {
         return listener.handleEvent;
       } else {
-        // to support Symbol
-        return function(event) {
-          listener(event);
-        };
+        return listener;
       }
     }
   };
